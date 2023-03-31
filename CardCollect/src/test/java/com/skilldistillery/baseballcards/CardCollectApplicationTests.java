dcb44833cfbfb8da0823a5878f225dabb025f107
Tom <tom.mcglone1@yahoo.com>
2023-03-31 16:43:53 -0400

    Create Boot REST project, connect JPA project dependecy

---
+++ CardCollect/src/test/java/com/skilldistillery/baseballcards/CardCollectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.baseballcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardCollectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
