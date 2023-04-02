2ef8c6e1d4d8d626779aecec9a75805cdc9242d6
Tom <tom.mcglone1@yahoo.com>
2023-04-02 11:39:50 -0400

    App properties updated for deployment

--- CardCollect/src/main/java/com/skilldistillery/baseballcards/services/CardServiceImpl.java
+++ CardCollect/src/main/java/com/skilldistillery/baseballcards/services/CardServiceImpl.java
@@ -11,17 +11,17 @@ import com.skilldistillery.baseballcards.entities.CardGrade;
 import com.skilldistillery.baseballcards.repositories.CardConditionRepository;
 import com.skilldistillery.baseballcards.repositories.CardGradeRepository;
 import com.skilldistillery.baseballcards.repositories.CardRepository;
+
 @Service
 public class CardServiceImpl implements CardService {
 
 	@Autowired
 	private CardRepository cardRepo;
-	
-	
+
 	@Override
 	public List<Card> listAllCards() {
 		return cardRepo.findAll();
-		
+
 	}
 
 	@Override
@@ -38,24 +38,26 @@ public class CardServiceImpl implements CardService {
 	@Override
 	public Card update(int cardId, Card card) {
 		Card original = cardRepo.findById(cardId);
-		original.setPlayerName(card.getPlayerName());
-		original.setNumber(card.getNumber());
-		original.setTeam(card.getTeam());
-		original.setBoxSet(card.getBoxSet());
-		original.setType(card.getType());
-		original.setImgURL(card.getImgURL());
-		original.setYear(card.getYear());
-		original.setAutographed(card.getAutographed());
-		original.setRookie(card.getRookie());
-		original.setManufacturer(card.getManufacturer());
-		original.setSaleValue(card.getSaleValue());
-		original.setTradeValue(card.getTradeValue());
-		original.setActive(card.isActive());
-		original.setSpNumber(card.getSpNumber());
-		original.setParallel(card.getParallel());
-		original.setCondition(card.getCondition());
-		if(card.getGrade() != null) {
-		original.setGrade(card.getGrade());
+		if (original != null) {
+			original.setPlayerName(card.getPlayerName());
+			original.setNumber(card.getNumber());
+			original.setTeam(card.getTeam());
+			original.setBoxSet(card.getBoxSet());
+			original.setType(card.getType());
+			original.setImgURL(card.getImgURL());
+			original.setYear(card.getYear());
+			original.setAutographed(card.getAutographed());
+			original.setRookie(card.getRookie());
+			original.setManufacturer(card.getManufacturer());
+			original.setSaleValue(card.getSaleValue());
+			original.setTradeValue(card.getTradeValue());
+			original.setActive(card.isActive());
+			original.setSpNumber(card.getSpNumber());
+			original.setParallel(card.getParallel());
+			original.setCondition(card.getCondition());
+			if (card.getGrade() != null) {
+				original.setGrade(card.getGrade());
+			}
 		}
 		return cardRepo.saveAndFlush(card);
 	}
