42d6160e26a5ff135d248f474c714d236c92b99b
Thomas McGlone <tom@Toms-MacBook-Pro.local>
2023-06-04 18:00:39 -0400

    form validation for create a card complete

--- ngCardCollect/src/app/components/single-card-view/single-card-view.component.ts
+++ ngCardCollect/src/app/components/single-card-view/single-card-view.component.ts
@@ -6,59 +6,52 @@ import { CardService } from 'src/app/services/card.service';
 @Component({
   selector: 'app-single-card-view',
   templateUrl: './single-card-view.component.html',
-  styleUrls: ['./single-card-view.component.css']
+  styleUrls: ['./single-card-view.component.css'],
 })
 export class SingleCardViewComponent {
-
   selected: Card | null = null;
   gradeNumber: number | null = null;
   editCard: Card | null = null;
 
   constructor(
     private currentRoute: ActivatedRoute,
-    private cardService: CardService,
-  ){}
+    private cardService: CardService
+  ) {}
 
   ngOnInit(): void {
-    let cardId = this.currentRoute.snapshot.paramMap.get("cardId");
-    if(cardId){
-
+    let cardId = this.currentRoute.snapshot.paramMap.get('cardId');
+    if (cardId) {
       this.findCardById(parseInt(cardId));
     }
   }
 
-  findCardById(cardId: number){
+  findCardById(cardId: number) {
     this.cardService.show(cardId).subscribe({
-      next:(card) =>{
+      next: (card) => {
         this.selected = card;
       },
-      error:(err) =>{
+      error: (err) => {
         console.error(err);
-      }
+      },
     });
   }
 
-  setEditCard(){
-
-
+  setEditCard() {
     this.editCard = Object.assign({}, this.selected);
-    this.gradeNumber  = this.editCard?.grade?.id !== undefined ? this.editCard?.grade?.id: null;
+    this.gradeNumber =
+      this.editCard?.grade?.id !== undefined ? this.editCard?.grade?.id : null;
   }
   updateCard(card: Card) {
     if (!this.gradeNumber) {
       card.grade = null;
     } else {
-      card.grade = { id: this.gradeNumber,
-      name: '' };
+      card.grade = { id: this.gradeNumber, name: '' };
     }
 
     this.cardService.update(card).subscribe({
       next: (updatedCard) => {
-        console.log(updatedCard)
         this.editCard = null;
-        this.findCardById(updatedCard.id)
-
-
+        this.findCardById(updatedCard.id);
       },
       error: (fail) => {
         console.error('Error updating card');
